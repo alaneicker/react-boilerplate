4bb80acacde72b4989ae224570e076eb298506a6
Alan Eicker <Alan.Eicker@allstate.com>
2019-08-19 14:53:34 -0500

    layout styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './assets/styles/app.scss';
 import Home from './components/home/';
 
 const App = () => {
   return (
-    <Fragment>
-      <Router>
-        <Route path="/" exact component={Home} />
-      </Router>
-    </Fragment>
+    <Router>
+      <div className="page">
+        <div className="page__hd">
+
+        </div>
+        <div className="page__bd">
+          <div className="page__content">
+            <Route path="/" exact component={Home} />
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 };
 
