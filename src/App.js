cedcffffb94ea08b0bd8ed56d4a6f996deecbe66
Alan Eicker <Alan.Eicker@allstate.com>
2019-08-19 11:43:42 -0500

    Adds home rout and node-sass dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Home from './components/home';
+
 const App = () => {
   return (
     <Fragment>
       <Router>
-        Hello world!!!
+        <Route path="/" exact component={Home} />
       </Router>
     </Fragment>
   );
