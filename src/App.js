8c5ccbf0683fc4a912878099b9c552f89d458944
Alan Eicker <Alan.Eicker@allstate.com>
2019-08-19 11:40:38 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Router>
+        Hello world!!!
+      </Router>
+    </Fragment>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const AutoPrefixer = require('autoprefixer');
 const TerserJsPlugin = require('terser-webpack-plugin');
@@ -11,101 +12,96 @@ const rootDir = path.resolve(__dirname);
 const srcDir = path.resolve(__dirname, 'src');
 const nodeModDir = path.resolve(__dirname, 'node_modules');
 const distDir = path.resolve(__dirname, 'dist');
+const publicDir = path.resolve(__dirname, 'public');
 
 const relativeDistJsDir = 'assets/js';
 const relativeDistStylesDir = 'assets/styles';
 
 const indexEntry = path.join(srcDir, 'index.js');
 
-const config = (env) => {
-  const prod = env.NODE_ENV === 'prod';
+const prod = process.env.NODE_ENV === 'prod';
 
-  const config = {
-    entry: [
-      indexEntry,
-    ],
-    output: {
-      path: distDir,
-      libraryTarget: 'umd',
-      globalObject: 'this',
-      filename: path.join(relativeDistJsDir, '[name].min.js'),
-    },
-    resolve: {
-      modules: [rootDir, nodeModDir],
-      extensions: ['.js', '.jsx'],
-    },
-    devtool: 'source-map',
-    target: 'web',
-    module: {
-      rules: [
-        {
-          test: /\.(jsx?)$/,
-          exclude: /[\\/]node_modules[\\/]/,
-          use: 'babel-loader',
-        },
-        {
-          test: /\.(s?css|sass)$/,
-          exclude: /[\\/]node_modules[\\/]/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                hmr: !prod,
-              },
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 2,
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: () => AutoPrefixer,
-              },
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
+const config = {
+  entry: [
+    indexEntry,
+  ],
+  output: {
+    path: distDir,
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    filename: path.join(relativeDistJsDir, '[name].min.js'),
+  },
+  resolve: {
+    modules: [rootDir, nodeModDir],
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx?)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: ['babel-loader'],
       },
-      ],
-    },
-    optimization: {
-      minimizer: [
-        new TerserJsPlugin({
-          cache: true,
-          parallel: true,
-          terserOptions: {
-            ecma: 6,
+      {
+        test: /\.(s?css|sass)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !prod,
+            },
           },
-          sourceMap: true,
-        }),
-        new OptimizeCssAssetsPlugin(),
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin([distDir]),
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      prod ? new webpack.HashedModuleIdsPlugin() : new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-      new MiniCssExtractPlugin({
-        filename: path.join(relativeDistStylesDir, '[name].css'),
-        chunkFilename: path.join(relativeDistStylesDir, '[name].css'),
-      }),
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => AutoPrefixer,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
-  };
-
-  if (!prod) {
-    config.devServer = {
-      contentBase: distDir,
-      port: 8080,
-    };
-  }
-
-  return config;
+  },
+  optimization: {
+    minimizer: [
+      new TerserJsPlugin({
+        cache: true,
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        },
+        sourceMap: true,
+      }),
+      new OptimizeCssAssetsPlugin(),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [distDir] }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    prod ? new webpack.HashedModuleIdsPlugin() : new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+    new MiniCssExtractPlugin({
+      filename: path.join(relativeDistStylesDir, '[name].css'),
+      chunkFilename: path.join(relativeDistStylesDir, '[name].css'),
+    }),
+    new HtmlWebpackPlugin({
+      hot: true,
+      inject: true,
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
 };
 
-module.export = config;
+module.exports = config;
