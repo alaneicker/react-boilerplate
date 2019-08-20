208b798296ef422c998b48e7b3075195054257be
Alan Eicker <alaneicker@gmail.com>
2019-08-19 20:38:57 -0500

    Styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './assets/styles/app.scss';
 import Home from './components/home/';
 
@@ -7,9 +7,15 @@ const App = () => {
   return (
     <Router>
       <div className="page">
-        <div className="page__hd">
-
-        </div>
+        <header className="page__hd">
+          <Link className="header-logo">Logo</Link>
+          <ul className="header-nav">
+            <li><Link to>Home</Link></li>
+            <li><Link to>About</Link></li>
+            <li><Link to>Services</Link></li>
+            <li><Link to>Contact Us</Link></li>
+          </ul>
+        </header>
         <div className="page__bd">
           <div className="page__content">
             <Route path="/" exact component={Home} />
