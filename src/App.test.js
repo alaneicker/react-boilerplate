9f6560f7f9ecf8647bc1263949b91f477daa85ca
Alan Eicker <Alan.Eicker@allstate.com>
2019-08-20 11:58:17 -0500

    Fixes tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
+import store from './reducers';
 
 import App from './App';
 
 describe('App', () => {
   it('Should render without errors', () => {
-    const component = shallow(<App />);
+    const component = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(component).toHaveLength(1);
   });
 });
--- src/components/splash/splash.test.js
+++ src/components/splash/splash.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
+import store from '../../reducers';
 
 import Splash from '.';
 
 describe('Splash', () => {
   it('Should render without errors', () => {
-    const component = shallow(<Splash />);
+    const component = shallow(
+      <Provider store={store}>
+        <Splash />
+      </Provider>
+    );
     expect(component).toHaveLength(1);
   });
 });
