e5bd459de3e1a566c1870fc34136a8c4875e46d8
Alan Eicker <alaneicker@hotmail.com>
2019-09-15 15:59:15 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
 
 import './assets/styles/app.scss';
 
-ReactDOM.hydrate(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root'),
-);
+ReactDOM.hydrate(<App />, document.getElementById('root'));
